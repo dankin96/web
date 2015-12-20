b22626ddb00dc8eb6a5ae48a0a0afdcaa4e12702
dankin96 <dankin96@gmail.com>
2015-12-21 02:16:16 +0300

    web_3_1

--- ask_danilov/ask_danilov/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.utils.html import escape
-def hello(request):
-    data = "Hello, world!!! </br>"
-    get = request.GET
-    post = request.POST
-    html = data + "</br>Your GET data:</br>" +  get.urlencode() + "</br>Your POST data:</br>"  + post.urlencode()
-    return HttpResponse((html))
